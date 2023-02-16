cc2e56ae808f8b8e7d59386dbaa883a134941449
ThejasNU <thejasnu@gmail.com>
2023-02-16 10:23:03 +0530

    changed hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"PES1UG20CS606"<<endl;
+  cout<<"PES1UG20CS606 Thejas"<<endl;
   return 0;
 }
